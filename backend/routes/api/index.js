ad46896382887c761ace313ba4b800dc44cb4864
sarah <sarahnmoore19@gmail.com>
2023-02-20 06:29:07 -0500

    add user auth middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,44 @@
 const router = require('express').Router();
+
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
 module.exports = router;
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+//
+// const { requireAuth } = require('../../utils/auth.js');
+//
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+//
+//
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
+//
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+//
+//
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+//
