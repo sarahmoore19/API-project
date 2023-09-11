5757db489228d441f9a6215838f451f59835f961
sarah <sarahnmoore19@gmail.com>
2023-09-11 17:15:06 -0400

    new spots fix error

--- backend/db/seeders/20230224161801-create-spot-images.js
+++ backend/db/seeders/20230224161801-create-spot-images.js
@@ -48,6 +48,51 @@ module.exports = {
       spotId: 3,
       url: 'https://www.hearthymn.com/wp-content/uploads/2018/02/Why-Is-the-Lord-Jesus-Called-Christ-1.jpg',
       preview: false
+    },
+    {
+      spotId: 4,
+      url: 'https://media.istockphoto.com/id/489833698/photo/two-lounge-chairs-under-tent-on-beach.jpg?s=612x612&w=0&k=20&c=df_6Jkb5YreyaAcGVbdcRrTP4dZ-opUrYWtgRC3eKKI=',
+      preview: true
+    },
+    {
+      spotId: 5,
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVUXbtm9U3aWKzW2wOMsWF9Jv5hgz9bHX_iw&usqp=CAU',
+      preview: true
+    },
+    {
+      spotId: 6,
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE1EnjMuG3gT1ObVgqfNVRO0mTGEHuAMrXtA&usqp=CAU',
+      preview: true
+    },
+    {
+      spotId: 7,
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdx7tj4q9NWbi2picMDDrd3spjS5XHPlm82YVuWLGohozhfULPr3wgG_Woh1yhkSrMSMY&usqp=CAU',
+      preview: true
+    },
+    {
+      spotId: 8,
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0mWumDEjEAepaRjnRRCuJFvkOMp3n0Ga9fA&usqp=CAU',
+      preview: true
+    },
+    {
+      spotId: 9,
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFVYzSQA1Z_dcysG3hUeyozjwAvSdhs_ohq_ErLdU2NprIftIylllQoXonYfCfxwCsQBs&usqp=CAU',
+      preview: true
+    },
+    {
+      spotId: 10,
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScOpuhFp7EECROUcISftB8M98vGMvZmXfUUA&usqp=CAU',
+      preview: true
+    },
+    {
+      spotId: 11,
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzVbwKVSqiwYEXEY-o7BDXsFWPTfNzVw92NA&usqp=CAU',
+      preview: true
+    },
+    {
+      spotId: 12,
+      url: 'https://media.vrbo.com/lodging/35000000/34150000/34145600/34145558/92beade2.c6.jpg',
+      preview: true
     }
    ], {})
   },
